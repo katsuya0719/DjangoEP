9b5d025ed43101f988cd06ef1e9e808f3469e737
katsuya0719 <katsuya0719@gmail.com>
2017-04-05 21:43:16 +0900

    Update documentation

--- heatBalance/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from heatBalance.models import heatBalance
-
-class heatBalSerializer(serializers.ModelSerializer):
-	class Meta:
-		model=heatBalance
-		fields=('id','project','sensibleH','sensibleC')
-
-	
