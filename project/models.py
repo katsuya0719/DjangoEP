83972e61b76b79746c8434922c0a1ae1f5d9c8d2
katsuya0719 <katsuya0719@gmail.com>
2017-04-06 09:09:30 +0900

    removed image file in 17013010 temporally

---
+++ project/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+def dir_path(instance,filename):
+	return 'html/{0}/{1}'.format(instance.project,filename)
+
+# Create your models here.
+class html(models.Model):
+	project=models.CharField(max_length=50,blank=True)
+	html=models.FileField(upload_to=dir_path)
+	uploaded_at=models.DateTimeField(auto_now_add=True)
+	
+	def __str__(self):
+		return self.project
+
+class basic(models.Model):
+	html=models.OneToOneField(html,primary_key=True)
+	total_area=models.IntegerField()
+	condition_area=models.IntegerField()
+	uncondition_area=models.IntegerField()
+
+class electricity(models.Model):
+	html=models.OneToOneField(html,primary_key=True)
+	file=models.FileField(upload_to='electricity')
+
